91c32781b2888d8184dd167f7a54e28296d69439
Björn Fischer <bjoern.fischer@inovex.de>
2020-07-15 15:35:36 +0200

    add check

---
+++ test/check.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+until [ $(kubectl get po test-application -o=jsonpath='{.status.containerStatuses[0].state.terminated.reason}') == "Completed" ];
+do
+  echo "Waiting for test pod to be completed. Current status is " $(kubectl get po test-application -o=jsonpath='{.status.containerStatuses[0].state}')
+  sleep 10s
+done
+
+echo "Test container completed"
