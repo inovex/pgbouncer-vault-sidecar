c643edbf5f2cdd95f59ef543e89a23bcc8bdec0c
Björn Fischer <bjoern.fischer@inovex.de>
2020-07-15 15:44:42 +0200

    avoid error if container has not completed

--- test/check.sh
+++ test/check.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-until [ $(kubectl get po test-application -o=jsonpath='{.status.containerStatuses[0].state.terminated.reason}') == "Completed" ];
+until [ "$(kubectl get po test-application -o=jsonpath='{.status.containerStatuses[0].state.terminated.reason}')" == "Completed" ];
 do
   echo "Waiting for test pod to be completed. Current status is " $(kubectl get po test-application -o=jsonpath='{.status.containerStatuses[0].state}')
   sleep 10s
