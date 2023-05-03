2c7668484f698e812637b307df8395e7f1800754
Björn Fischer <bjoern.fischer@inovex.de>
2023-05-03 09:20:34 +0200

    Fix if-statement

--- test/run.sh
+++ test/run.sh
@@ -10,7 +10,7 @@ set -ex
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 
-if [ "$GITHUB_ACTIONS" == "true" ]
+if [ "$GITHUB_ACTIONS" == "true" ]; then
     echo Waiting 10 seconds to make sure Minikube DNS is ready
     sleep 10
 fi
